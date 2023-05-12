a2af980b51b15ad7f67e4a919e74e2fd42552017
Ysgrmr13 <126651009+Ysgrmr13@users.noreply.github.com>
2023-05-12 12:46:36 +0300

    Heroku: Update database configuration from $DATABASE_URL.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
